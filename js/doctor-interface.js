91ebdda762d6d0dacedeeef49f823d7492da8f44
Starlynne <lewis.start0905@gmail.com>
2017-05-26 16:51:11 -0700

    Add styling, list of viewed doctors, api link.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,8 +4,10 @@ var displayInfo = function(doctors){
   $('.return').text('');
   $('.error').hide()
   doctors.forEach(function(doctor) {
-    $('.return').append('<li><a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwi4rOCxwY7UAhUoxlQKHfvkC6sQPAgD#hl=en&q=dr+' + doctor.firstName + '+' + doctor.lastName + '" target="_blank"><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3></a><ul><li><img src="' + doctor.image + '" alt="No image available."</li><br><br><li>Bio: ' + doctor.bio + '</li><li>Specialty:<ul><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></ul></li><br><h4 class="info">Contact Information:</h4><div class="contact"><ul><li>' + doctor.practice + '</li><li>Distance:' + doctor.distance + '</li><li><ul><li>' + doctor.address +'</li><li>' + doctor.city + '</li><li>'+ doctor.state +'</li><li>Tel:' + doctor.phone + '</li></ul></li></ul>');
+    $('.return').append('<li class="docs"><a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwi4rOCxwY7UAhUoxlQKHfvkC6sQPAgD#hl=en&q=dr+' + doctor.firstName + '+' + doctor.lastName + '" target="_blank"><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3></a><ul><li><img src="' + doctor.image + '" alt="No image available."></li><br><br><li class="well bio">Bio: ' + doctor.bio + '</li><li class="well special">Specialty:<ul ><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></li><br><h4 class="info">Contact Information:</h4><div class="contact well"><ul><li>' + doctor.practice + '</li><li>Address: (Distance:' + doctor.distance + ' mi)</li><ul><li>' + doctor.address +'</li><li>' + doctor.city + '</li><li>'+ doctor.state +'</li><li>Tel:' + doctor.phone + '</li></ul></ul><br><li>*note-many doctors work at more than one practice. Only the first is shown.</li></div></ul>');
+    $('#doctorList').append('<li>'+ doctor.lastName + ', ' + doctor.specialtyName + '</li><br>')
   });
+
 };
 
 $(document).ready(function(){
