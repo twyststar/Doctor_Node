c1d2b22a05ada1c5a845c1a57e16ce978ab392d5
Starlynne <lewis.start0905@gmail.com>
2017-05-26 13:04:10 -0700

    Add contact info display, fix bug on return length

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,8 +2,9 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayInfo = function(doctors){
   $('.return').text('');
+  $('.error').hide()
   doctors.forEach(function(doctor) {
-    $('.return').append('<li><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3><ul><li><img src="' + doctor.image + '" alt="No image available."</li><br><br><li>Bio: ' + doctor.bio + '</li><li>Specialty:<ul><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></ul></li><br>');
+    $('.return').append('<li><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3><ul><li><img src="' + doctor.image + '" alt="No image available."</li><br><br><li>Bio: ' + doctor.bio + '</li><li>Specialty:<ul><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></ul></li><br><button class="info">Contact Information</button><div><ul><li>' + doctor.practice + '</li><li>Distance:' + doctor.distance + '</li><li><ul><li>' + doctor.address +'</li><li>' + doctor.city + '</li><li>'+ doctor.state +'</li><li>Tel:' + doctor.phone + '</li></ul></li></ul>');
   });
 };
 
--- js/doctor.js
+++ js/doctor.js
@@ -1,6 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(lastName, firstName, title, image, bio, specialtyName, specialtyDesc, distance, city, state, address, phone ) {
+function Doctor(lastName, firstName, title, image, bio, specialtyName, specialtyDesc, distance, practice, city, state, address, phone ) {
   this.lastName = lastName;
   this.firstName = firstName;
   this.title = title;
@@ -9,6 +9,7 @@ function Doctor(lastName, firstName, title, image, bio, specialtyName, specialty
   this.specialtyName = specialtyName;
   this.specialtyDesc = specialtyDesc;
   this.distance = distance
+  this.practice = practice
   this.city = city;
   this.state = state;
   this.address = address;
@@ -23,9 +24,10 @@ Doctor.prototype.getInfo = function(search, sort, displayInfo) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ search + '&location='+ latLong +'100&user_location='+ userLoc + '&sort='+ sort + '&skip=0&limit=10&user_key=' + apiKey).then(function(response){
     console.log(response)
     if (response.data.length === 0) {
+      $('.return').text('');
       $('.error').show()
     } else {
-      for (var i =0; i < 10; i++) {
+      for (var i =0; i < response.data.length; i++) {
         if (response.data[i].specialties.length >= 1) {
           var  specName = response.data[i].specialties[0].name;
           var specDesc = response.data[i].specialties[0].description;
@@ -33,13 +35,20 @@ Doctor.prototype.getInfo = function(search, sort, displayInfo) {
           var  specName = 'No specialty info available.';
           var specDesc = 'No specialty description available.';
         }
+        var distance = response.data[i].practices[0].distance.toFixed(1)
         var image = response.data[i].profile.image_url;
         var lastName = response.data[i].profile.last_name;
         var firstName = response.data[i].profile.first_name;
         var title = response.data[i].profile.title;
         var bio = response.data[i].profile.bio;
+        var practice = response.data[i].practices[0].name
+        var city = response.data[i].practices[0].visit_address.city
+        var state = response.data[i].practices[0].visit_address.state
+        var address = response.data[i].practices[0].visit_address.street
+        var phone = response.data[i].practices[0].phones[0].number
 
-        newDoc = new Doctor(lastName, firstName, title, image, bio, specName, specDesc)
+
+        newDoc = new Doctor(lastName, firstName, title, image, bio, specName, specDesc, distance, practice, city, state, address, phone)
         doctors.push(newDoc);
       }
       console.log(doctors)
