ffeedd7baf0db552123ea063a717378942c71ebd
Starlynne <lewis.start0905@gmail.com>
2017-05-26 14:56:21 -0700

    Add location select fields, add styling, make result names link to google search.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,7 +4,7 @@ var displayInfo = function(doctors){
   $('.return').text('');
   $('.error').hide()
   doctors.forEach(function(doctor) {
-    $('.return').append('<li><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3><ul><li><img src="' + doctor.image + '" alt="No image available."</li><br><br><li>Bio: ' + doctor.bio + '</li><li>Specialty:<ul><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></ul></li><br><button class="info">Contact Information</button><div><ul><li>' + doctor.practice + '</li><li>Distance:' + doctor.distance + '</li><li><ul><li>' + doctor.address +'</li><li>' + doctor.city + '</li><li>'+ doctor.state +'</li><li>Tel:' + doctor.phone + '</li></ul></li></ul>');
+    $('.return').append('<li><a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwi4rOCxwY7UAhUoxlQKHfvkC6sQPAgD#hl=en&q=dr+' + doctor.firstName + '+' + doctor.lastName + '" target="_blank"><h3> Dr. ' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</h3></a><ul><li><img src="' + doctor.image + '" alt="No image available."</li><br><br><li>Bio: ' + doctor.bio + '</li><li>Specialty:<ul><li>'+ doctor.specialtyName + '</li><li>' + doctor.specialtyDesc + '</li><li>*note- doctors may have many specialties. Only the first is listed.</li></ul></ul></li><br><h4 class="info">Contact Information:</h4><div class="contact"><ul><li>' + doctor.practice + '</li><li>Distance:' + doctor.distance + '</li><li><ul><li>' + doctor.address +'</li><li>' + doctor.city + '</li><li>'+ doctor.state +'</li><li>Tel:' + doctor.phone + '</li></ul></li></ul>');
   });
 };
 
@@ -15,6 +15,13 @@ $(document).ready(function(){
     var search = $('#search').val();
     $('#search').val('');
     var sort = $('#sort').val()
-    docSearch.getInfo(search, sort, displayInfo);
+    var userLoc = $('#location').val()
+    var latLong = (userLoc + '%2c')
+    docSearch.getInfo(userLoc, latLong, search, sort, displayInfo);
+
+    $('.info').click(function(){
+      $(this).next('contact').slideToggle();
+    });
   });
+
 });
